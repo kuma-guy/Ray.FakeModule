d753fbe20b7ce524c093a3c910964430d735c602
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-22 12:26:54 +0900

    add @Fakeable annotation

---
+++ src/Annotation/Fakeable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Ray.FakeModule
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\FakeModule\Annotation;
+
+/**
+ * @Annotation
+ * @Target("CLASS")
+ */
+final class Fakeable
+{
+}
